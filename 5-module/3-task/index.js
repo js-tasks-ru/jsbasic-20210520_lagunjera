3c113b1d5d0c5b617ce830c5df9da8de63d24086
Lagunjera <lagunjera@yandex.ru>
2021-06-11 11:22:01 +0300

    Решения: модуль 5 (Обработчики событий)

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,47 @@
 function initCarousel() {
-  // ваш код...
+  let arrowLeft = document.querySelector(".carousel__arrow_left");
+  let arrowRight = document.querySelector(".carousel__arrow_right");
+  let slideWidth = document.querySelector(".carousel__slide").offsetWidth;
+  let numberOfSlides = document.querySelectorAll(".carousel__slide").length;
+  let maxShiftWidth = slideWidth * (numberOfSlides - 1);
+  let carouselInner = document.querySelector(".carousel__inner");
+  let position = 0;
+
+
+  arrowLeft.style.display = "none";
+
+  document.addEventListener("click", function(event) {
+    if (event.target.tagName == "IMG" &&
+        event.target.parentElement.classList.contains("carousel__arrow_right") &&
+        position < maxShiftWidth) {
+      
+      arrowLeft.style.display = "";
+
+      position += slideWidth;
+      carouselInner.style.transform = `translateX(-${position}px)`;
+
+      if (position == maxShiftWidth) {
+        arrowRight.style.display = "none";
+      }
+    
+    } 
+    
+    if (event.target.tagName == "IMG" &&
+        event.target.parentElement.classList.contains("carousel__arrow_left")) {
+
+      arrowRight.style.display = "";
+      
+      position -= slideWidth;
+      carouselInner.style.transform = `translateX(-${position}px)`;
+      
+      if (position == 0) {
+        arrowLeft.style.display = "none";
+      }
+    }
+  });
+  
+
+  
+
+
 }
